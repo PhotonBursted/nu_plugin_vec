dec915ed9ca3a69d243fa7b319f6bbd144af9770
PhotonBursted <inquiries@photonbur.st>
2024-10-20 20:16:06 +0000

    test(commands): add checks for errors when supplying uneven vectors
    Signed-off-by: PhotonBursted <inquiries@photonbur.st>

--- tests/commands/sub.rs
+++ tests/commands/sub.rs
@@ -1,6 +1,28 @@
 use nu_plugin_test_support::PluginTest;
 use nu_plugin_vec::commands::Sub as Command;
-use nu_plugin_vec::{test_examples, VecPlugin};
+use nu_plugin_vec::{nu, test_examples, VecPlugin};
 use nu_protocol::ShellError;
 
 test_examples!();
+
+#[test]
+fn errors_when_left_vector_is_shorter() {
+    let result = nu!("\
+                let a = [1 2];\
+                let b = [1 2 3 4 5];\
+                \
+                $a | vec sub $b");
+
+    assert!(result.is_err());
+}
+
+#[test]
+fn errors_when_right_vector_is_shorter() {
+    let result = nu!("\
+                let a = [1 2 3 4 5];\
+                let b = [1 2 3];\
+                \
+                $a | vec sub $b");
+
+    assert!(result.is_err());
+}
